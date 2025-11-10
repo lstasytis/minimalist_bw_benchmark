cf3cfaef58ef9f4c4f6298528f0b96c932718e42
Lukas Stasytis <l.stasytis1@gmail.com>
2025-11-10 09:59:18 +0100

    add pthread example of more precise measurement

--- run_pthread.sh
+++ run_pthread.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-
 #sudo sysctl -w kernel.perf_event_paranoid=0
 mode=$1
 
@@ -8,7 +7,8 @@ mode=$1
 if [ "$mode" == "1" ]; then
     echo "[-O0 basic]"
     export OMP_NUM_THREADS=1
-    g++ -std=c++20 -O0 -fopenmp -fverbose-asm -save-temps bw_test.cpp -o bw_test
+    g++ -std=c++20 -O3 -fopenmp -pthread -march=native \
+        -ftree-vectorize -funroll-loops bw_test_pthread.cpp -o bw_test_pthread
 fi
 
 # O3+SIMD
@@ -16,7 +16,7 @@ if [ "$mode" == "2" ]; then
     echo "-O3 vectorized]"
     export OMP_NUM_THREADS=1
     g++ -std=c++20 -O3 -fopenmp -march=native -ftree-vectorize -funroll-loops \
-        -fverbose-asm -save-temps bw_test.cpp -o bw_test
+        -fverbose-asm -save-temps bw_test_pthread.cpp -o bw_test_pthread
 fi
 
 # O3+SIMD+OMP (show with and without the pragma)
@@ -24,12 +24,11 @@ if [ "$mode" == "3" ]; then
     echo "-O3 vectorized + OMP]"
     export OMP_NUM_THREADS=$(nproc)
     g++ -std=c++20 -O3 -fopenmp -march=native -ftree-vectorize -funroll-loops \
-        -fverbose-asm -save-temps bw_test.cpp -o bw_test
+        -fverbose-asm -save-temps bw_test_pthread.cpp -o bw_test_pthread
 fi
 
 #./bw_test
-perf stat ./bw_test
+perf stat ./bw_test_pthread
 
-# useful commands to look at
-# lscpu
+# useful commands to look at# lscpu
 # sudo lshw -class memory
